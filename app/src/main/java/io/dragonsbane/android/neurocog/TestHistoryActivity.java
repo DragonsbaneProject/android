5710eae1f7cc384632e1e8564ce2421544169b78
Brian Taylor <objectorange@protonmail.com>
2018-05-26 09:36:38 -0400

    add api call

--- app/src/main/java/io/dragonsbane/android/neurocog/TestHistoryActivity.java
+++ app/src/main/java/io/dragonsbane/android/neurocog/TestHistoryActivity.java
@@ -24,7 +24,7 @@ public class TestHistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_history);
-        
+        HealthRecordAPI.loadMemoryTestHistory(getApplicationContext(), ((DBApplication)getApplication()).getDid());
     }
 
 //    public void sendHistory(View view) {
